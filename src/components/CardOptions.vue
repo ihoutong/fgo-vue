44f8c1198090ad30ddd903b327384cb3b9a301f1
= <ihoutong93@gmail.com>
2018-03-25 00:01:27 -0400

    Servant list with basic filter for command cards/np

---
+++ src/components/CardOptions.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-sm">
+    {{display_name}}
+    <select v-model="dropdown_value">
+      <option v-for="(value, key) in values" v-bind:value="value">{{key}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardOptions',
+  props: {
+    display_name: String,
+    values: Object,
+  },
+  data: function () {
+    return {
+      dropdown_value: ''
+    }
+  },
+  watch: {
+    dropdown_value (val){
+      this.$emit('input', val);
+    }
+  }
+}
+</script>
---
+++ src/components/ServantList.vue
@@ -0,0 +1,217 @@
+<template>
+  <div>
+    <div class="filter-container">
+      <div class="pointer" v-on:click="show_filters()">
+        Filters &#709;
+      </div>
+      <div v-bind:class="{'d-none': hide_filters}">
+        Card Distribution
+        <div class="row">
+          <CardOptions v-bind:display_name="'Number of Buster'" v-bind:values="cardoptions_values" v-model="buster"/>
+          <CardOptions v-bind:display_name="'Number of Quick'" v-bind:values="cardoptions_values" v-model="quick"/>
+          <CardOptions v-bind:display_name="'Number of Arts'" v-bind:values="cardoptions_values" v-model="arts"/>
+          <CardOptions v-bind:display_name="'Np Type'" v-bind:values="np_options" v-model="np_type" />
+        </div>
+      </div>
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Rarity</th>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Class</th>
+          <th>Cost</th>
+          <th>Base HP</th>
+          <th>Base Attack</th>
+          <th>Base HP</th>
+          <th>Max HP</th>
+          <th>Command Cards</th>
+          <th>NP</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="servant in servants">
+          <td v-for="field in display_fields">
+              <span v-if="typeof servant[field] === 'string' && servant[field].indexOf('.png') !== -1">
+                <img v-bind:src="require('../'+servant[field])" alt="">
+              </span>
+              <span v-else-if="field === 'cardIds'">
+                <img style="margin-right: -40px;" v-for="card in servant[field]" v-bind:src="require('../assets/images/cmdCard/'+card+'.png')" alt="">
+              </span>
+              <span v-else>{{servant[field]}}</span>
+          </td>
+          <!-- <td v-for="(value, key) in servant">
+            <span v-if="typeof value === 'string' && value.indexOf('.png') !== -1">
+              <img v-bind:src="require('../'+value)" alt="">
+            </span>
+            <span v-else-if="key === 'cardIds'">
+              <img style="margin-right: -40px;" v-for="card in value" v-bind:src="require('../assets/images/cmdCard/'+card+'.png')" alt="">
+            </span>
+            <span v-else>{{value}}</span>
+          </td> -->
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import servant from '../assets/servant.json';
+import servant_np from '../assets/servant_np.json';
+import servant_stat from '../assets/servant_stat.json';
+import classes from '../assets/class.json';
+import CardOptions from './CardOptions.vue';
+
+//add class filter
+//
+export default {
+  name: 'HelloWorld',
+  components: {
+    CardOptions
+  },
+  data: function () {
+    return {
+      servants_original: [],
+      servant_json: {},
+      cardoptions_values: {
+        0:0,
+        1:1,
+        2:2,
+        3:3
+      },
+      np_options: {
+        Any: 0,
+        Buster: 2,
+        Quick: 3,
+        Arts: 1
+      },
+      buster: 0,
+      quick: 0,
+      arts: 0,
+      np_type: 0,
+      display_fields: ['collectionNo', 'rarity', 'image', 'name', 'class', 'cost', 'hpBase', 'atkBase', 'hpMax', 'atkMax', 'cardIds', 'np'],
+      hide_filters: true
+    }
+  },
+  created: function (){
+    //require('../assets/images/cmdCard/'+card+'.png')
+
+    //var fields_to_store = ['collectionNo', 'cardIds', 'id', 'name', 'classId', 'cost'];
+    for (var i = 0; i < servant.length; i++){
+      var temp_obj = {};
+
+      //For now just pull the cardid. In the future, pull the damage distribution too. Maybe even the strengthening availability
+      var np = servant_np.find(obj => {
+        return obj.num == 1 && obj.svtId == servant[i].id
+      });
+
+      var stat = servant_stat.find(obj => {
+        return obj.svtId == servant[i].id
+      });
+
+      var class_data = classes.find(obj => {
+        return obj.id == servant[i].classId;
+      });
+
+      for (var x = 0; x < this.display_fields.length; x++){
+        var field_name = this.display_fields[x];
+        if (['rarity', 'hpBase', 'atkBase', 'hpMax', 'atkMax'].indexOf(field_name) !== -1){
+          temp_obj[field_name] = stat[field_name];
+        }
+        else if (field_name == 'np'){
+          temp_obj[field_name] = 'assets/images/cmdCard/'+np.cardId+'.png'
+        }
+        else if (field_name == 'class'){
+          temp_obj[field_name] = 'assets/images/class/class_'+class_data.iconImageId+'_'+temp_obj.rarity+'.png';
+        }
+        else if (field_name == 'image'){
+          temp_obj[field_name] = 'assets/images/faces/'+servant[i].id+'0.png';
+        }
+        else{
+          temp_obj[this.display_fields[x]] = servant[i][this.display_fields[x]];
+        }
+      }
+      temp_obj.collectionNo = parseInt(temp_obj.collectionNo);
+      temp_obj['np_card'] = np.cardId;
+
+      // temp_obj['rarity'] = stat.rarity;
+      // temp_obj['np_card'] = 'assets/images/cmdCard/'+np.cardId+'.png';
+      // temp_obj['hp_base'] = stat.hpBase;
+      // temp_obj['hp_max'] = stat.hpMax;
+      // temp_obj['atk_base'] = stat.atkBase;
+      // temp_obj['atk_max'] = stat.atkMax;
+      // temp_obj['class_img'] = 'assets/images/class/class_'+class_data.iconImageId+'_'+temp_obj.rarity+'.png';
+      // temp_obj['np'] = np;
+      // temp_obj['stats'] = stat
+      // temp_obj['servant'] = servant[i];
+      // temp_obj['class'] = class_data;
+
+      this.servants_original.push(temp_obj);
+    }
+
+    this.servants_original.sort((a, b) => {
+      if (a.collectionNo < b.collectionNo){
+        return -1;
+      }
+      else if (a.collectionNo > b.collectionNo){
+        return 1;
+      }
+      return 0;
+    });
+    /* Servants have a type of 1,2,5,9 or 99 and a collectionNo
+    var type_arr = [1, 2, 5, 9, 99];
+    this.servants = servant.filter(function (a){
+      return type_arr.includes(a.type) && a.collectionNo != 0;
+    });*/
+  },
+  computed: {
+    servants: function (){
+      var cards_selected = [];
+      for (let i = 0; i < this.buster; i++){
+        cards_selected.push(2);
+      }
+      for (let i = 0; i < this.quick; i++){
+        cards_selected.push(3);
+      }
+      for (let i = 0; i < this.arts; i++){
+        cards_selected.push(1);
+      }
+
+      return this.servants_original.filter(obj => {
+        var cards_copy = cards_selected.slice();
+        for (var i = 0; i < obj.cardIds.length; i++){
+          if (cards_copy.length == 0 && (this.np_type == 0 || this.np_type == obj.np_card)){
+            return true;
+          }
+          var index_of = cards_copy.indexOf(obj.cardIds[i]);
+          if (index_of == -1){
+            continue;
+          }
+
+          cards_copy.splice(index_of, 1);
+        }
+
+        return cards_copy.length == 0 && (this.np_type == 0 || this.np_type == obj.np_card);
+      });
+    }
+  },
+  methods: {
+    show_filters: function (){
+      console.log('dasdasd');
+      this.hide_filters = !this.hide_filters;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+img {
+  width: 62px;
+  height: auto;
+}
+.filter-container {
+  padding-bottom: 10px;
+}
+</style>
